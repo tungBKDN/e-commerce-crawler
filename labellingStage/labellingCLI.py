7c5bde45241b6276de70ce0f2d325e26dfa82283
Tran Thanh Tung <102210138@sv1.dut.udn.vn>
2025-05-21 14:10:43 +0700

    feat: before set confidence > 0.4 to 1

--- labellingStage/labellingCLI.py
+++ labellingStage/labellingCLI.py
@@ -30,6 +30,12 @@ df = model.predict_dataframe(df)
 satisfied = df[(df['confidence'] >= 0.4) & (df['confidence'] < 1)]
 print(f"Number of confident samples: {len(satisfied)}")
 
+# Rows that has confidence > 0.4 and labeled is null, set label same as the predicted and set labeled to True then print the number of samples
+print(f"Number of samples that will be labeled: {len(df[(df['confidence'] >= 0.4) & (df['labeled'].isnull())])}")
+df.loc[(df['confidence'] >= 0.4) & (df['labeled'].isnull()), 'label'] = df['predicted_label']
+df.loc[(df['confidence'] >= 0.4) & (df['labeled'].isnull()), 'labeled'] = True
+df.loc[(df['confidence'] >= 0.4), 'confidence'] = 1
+
 # Saving
 df.to_csv("./data/LABEL_clean_comments.csv", index=False)
 u.plot_confidence(df)
